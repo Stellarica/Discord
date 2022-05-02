6ead7cf9fa30861611e4d7da5556d7850be62f93
trainb0y1 <66213737+trainb0y1@users.noreply.github.com>
2022-05-01 21:47:11 -0600

    Some punishment database progress

--- src/main/kotlin/io/github/hydrazinemc/bot/database/PunishmentLog.kt
+++ src/main/kotlin/io/github/hydrazinemc/bot/database/PunishmentLog.kt
@@ -1,14 +1,78 @@
 package io.github.hydrazinemc.bot.database
 
+import com.kotlindiscord.kord.extensions.commands.converters.impl.FormattedTimestamp
+import dev.kord.common.entity.Snowflake
+import dev.kord.core.cache.data.GuildData
+import dev.kord.core.entity.Guild
+import dev.kord.core.entity.User
 import org.jetbrains.exposed.dao.id.LongIdTable
+import org.jetbrains.exposed.sql.select
+import org.jetbrains.exposed.sql.transactions.transaction
+import java.time.Instant
 
 
 object PunishmentLogTable: LongIdTable() {
 	val guild = varchar("guild", 256) // The guild this took place in
-	val duration = integer("duration") // punishment duration in seconds
+	val expireTime = long("expireTime") // punishment expiration time
 	val timeApplied = long("timeApplied") // time punishment was applied
 	val reason = varchar("reason", 256)
 	val type = varchar("type", 256) // either WARN, MUTE, TIMEOUT, or BAN
 	val punisher = varchar("punisher", 256) // ID of the person who applied the punishment
 	val target = varchar("target", 256) // ID of the person who was punished
+}
+
+
+private fun logPunishmentToDatabase(
+	data: Punishment
+) {
+
+}
+
+/**
+ * Logs a punishment to the database, and
+ * logs it in the guild's configured channel
+ */
+fun logPunishment(data: Punishment) {
+
+}
+
+
+
+
+data class Punishment(
+	val guild: Snowflake,
+	val punisher: Snowflake,
+	val target: Snowflake,
+	val type: PunishmentType,
+	val reason: String,
+	val expireTime: Instant,
+	val timeApplied: Instant) {
+	val expired: Boolean
+		get() = expireTime.isBefore(Instant.now())
+}
+
+/**
+ * Global (cross-guild) punishments for this user
+ */
+val User.punishments: Set<Punishment>
+	get() = transaction {
+			val punishments = mutableListOf<Punishment>()
+			PunishmentLogTable.select { PunishmentLogTable.target eq this@punishments.id.value.toString() }.forEach {row ->
+				punishments.add(
+					Punishment(
+						Snowflake(row[PunishmentLogTable.guild]),
+						Snowflake(row[PunishmentLogTable.punisher]),
+						Snowflake(row[PunishmentLogTable.target]),
+						PunishmentType.valueOf(row[PunishmentLogTable.type]),
+						row[PunishmentLogTable.reason],
+						Instant.ofEpochSecond(row[PunishmentLogTable.expireTime]),
+						Instant.ofEpochSecond(row[PunishmentLogTable.timeApplied]),
+					)
+				)
+			}
+			return@transaction punishments.toSet()
+		}
+
+enum class PunishmentType {
+	WARN, MUTE, TIMEOUT, KICK, BAN
 }
--- src/main/kotlin/io/github/hydrazinemc/bot/extensions/ModerationExtension.kt
+++ src/main/kotlin/io/github/hydrazinemc/bot/extensions/ModerationExtension.kt
@@ -3,13 +3,23 @@ package io.github.hydrazinemc.bot.extensions
 import com.kotlindiscord.kord.extensions.checks.hasPermission
 import com.kotlindiscord.kord.extensions.commands.Arguments
 import com.kotlindiscord.kord.extensions.commands.application.slash.converters.impl.stringChoice
+import com.kotlindiscord.kord.extensions.commands.converters.impl.FormattedTimestamp
+import com.kotlindiscord.kord.extensions.commands.converters.impl.channel
+import com.kotlindiscord.kord.extensions.commands.converters.impl.defaultingTimestamp
+import com.kotlindiscord.kord.extensions.commands.converters.impl.snowflake
 import com.kotlindiscord.kord.extensions.commands.converters.impl.string
+import com.kotlindiscord.kord.extensions.commands.converters.impl.timestamp
+import com.kotlindiscord.kord.extensions.commands.converters.impl.user
 import com.kotlindiscord.kord.extensions.extensions.Extension
 import com.kotlindiscord.kord.extensions.extensions.publicSlashCommand
+import com.kotlindiscord.kord.extensions.time.TimestampType
 import com.kotlindiscord.kord.extensions.types.respond
 import dev.kord.common.entity.Permission
 import io.github.hydrazinemc.bot.database.GuildConfigTable
+import io.github.hydrazinemc.bot.database.botLogChannel
+import io.github.hydrazinemc.bot.database.punishmentLogChannel
 import io.github.hydrazinemc.bot.database.setGuildConfig
+import kotlinx.datetime.Instant
 
 class ModerationExtension : Extension() {
 	override val name: String = "moderation"
@@ -21,16 +31,28 @@ class ModerationExtension : Extension() {
 
 			check { hasPermission(Permission.ManageGuild) }
 			action {
-				/*, when (arguments.option) {
-					"pc" -> GuildConfigTable.punishmentLogChannel
-					"blc" -> GuildConfigTable.botLogChannel
+				// ngl this seems like purified jank
+				var thing = when (arguments.option) {
+					"pc" -> guild!!.punishmentLogChannel
+					"blc" -> guild!!.botLogChannel
 					else -> {
 						respond { content = "Somehow you chose an invalid option. This shouldn't be possible, and is a bug" }
 						return@action
 					}
-				}, arguments.value)
+				}
+				thing = arguments.value.id
+				respond { content = "Set ${arguments.option} to ${arguments.value.mention}" }
+			}
+		}
 
-				 */
+		publicSlashCommand(::ModerationCommandArgs) {
+			name = "mute"
+			description = "Mute a user"
+
+			// TODO: change this to a role; people might want to let mods use this but not discord builtin mute
+			check { hasPermission(Permission.MuteMembers) }
+			action {
+				respond { content = "Not yet implemented" }
 			}
 		}
 	}
@@ -44,9 +66,25 @@ class ModerationExtension : Extension() {
 				"bot-log-channel" to "blc"
 			)
 		}
-		val value by string {
+		val value by channel {
 			name = "value"
 			description = "The new value"
+		}
+	}
+
+	inner class ModerationCommandArgs : Arguments() {
+		val subject by user {
+			name = "user"
+			description = "The user in question"
+		}
+		val reason by string {
+			name = "reason"
+			description = "The reasoning behind this action"
+		}
+		val expireTime: FormattedTimestamp? by defaultingTimestamp {
+			name = "expire"
+			description = "The time at which this action expires"
+			defaultValue = FormattedTimestamp(Instant.DISTANT_FUTURE, TimestampType.ShortDateTime)
 		}
 	}
 }
