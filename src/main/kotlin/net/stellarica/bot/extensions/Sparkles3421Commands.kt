e3e0d2b44116c29bd0421b31827449a767c5c641
sparkles3421 <101483586+sparkles3421@users.noreply.github.com>
2023-01-21 21:26:47 +0000

    AGHHH

--- src/main/kotlin/net/stellarica/bot/extensions/Sparkles3421Commands.kt
+++ src/main/kotlin/net/stellarica/bot/extensions/Sparkles3421Commands.kt
@@ -1,5 +1,4 @@
 package net.stellarica.bot.extensions
-
 import com.kotlindiscord.kord.extensions.checks.hasPermission
 import com.kotlindiscord.kord.extensions.commands.Arguments
 import com.kotlindiscord.kord.extensions.commands.application.slash.converters.impl.stringChoice
@@ -11,53 +10,46 @@ import com.kotlindiscord.kord.extensions.utils.capitalizeWords
 import dev.kord.common.entity.Permission
 import dev.kord.rest.builder.message.create.embed
 import net.stellarica.bot.logger
-
 class BotStatusExtension : Extension() {
-	override val name: String = "status"
+    override suspend fun setup() {
+        publicSlashCommand(::SlapSlashArgs) {
+            name = "info"
+            description = "Get Info"
+            guild(TEST_SERVER_ID)
+            action {
+                val kord = this@TestExtension.kord
+                val realTarget = if (arguments.target.id == kord.selfId) {
+                    member
+                } else {
+                    arguments.target
+                }
+                var con = "An internal error has occured"
+                if (arguments.target) {
+                    if (arguments.target == "website") {
+                        con = """The stellarica website is: https://stellarica.net
+                            The wiki is: https://wiki.stellarica.net
+                            The information panel is: https://sparkles3421.github.io/stellarica/info/
+                        """.trimMargin()
+                    } else if (arguments.target == "discord") {
+                        con = "The discord is https://discord.com/invite/sjMY88Wwf8"
+                    } else {
+                        con = "Please set target to 'website' or 'discord'!"
+                    }
+                } else {
+                    con = "Please set a target"
+                }
+                respond {
+                    content = con;
+                }
+            }
+        }
+    }
+    inner class SlapSlashArgs : Arguments() {
+        val weapon by defaultingString {
+            name = "target"
+            description = "website/discord"
+            defaultValue = "website"
+        }
+    }
 
-	override suspend fun setup() {
-		publicSlashCommand(::StatusCommandArgs) {
-			name = "status"
-			description = "Set the bot status"
-
-			check { hasPermission(Permission.ManageGuild) }
-			action {
-				this@publicSlashCommand.kord.editPresence {
-					when (arguments.type) {
-						"watching" -> watching(arguments.text)
-						"listening" -> listening(arguments.text)
-						"playing" -> playing(arguments.text)
-					}
-				}
-				logger.info { "Set bot status to ${arguments.type} ${arguments.text}" }
-				respond {
-					embed {
-						this.title = "Bot Status Set"
-						this.description = arguments.type.capitalizeWords() +
-								if (arguments.type == "listening") {
-									" to "
-								} else {
-									" "
-								} + arguments.text
-					}
-				}
-			}
-		}
-	}
-
-	inner class StatusCommandArgs : Arguments() {
-		val type by stringChoice {
-			name = "type"
-			description = "status type"
-			choices = mutableMapOf(
-				"watching" to "watching",
-				"playing" to "playing",
-				"listening" to "listening"
-			)
-		}
-		val text by string {
-			name = "text"
-			description = "the text to set the status to"
-		}
-	}
 }
